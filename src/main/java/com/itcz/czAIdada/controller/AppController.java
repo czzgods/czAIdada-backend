c2ddf279de6fda91cc03d1b0d81c972fb99bab0f
cz <3302076969@qq.com>
2024-08-08 17:10:13 +0800

    使用代码生成器功能生成新增实体类的controller层，service层代码，并对其进行优化

--- src/main/java/com/itcz/czAIdada/controller/AppController.java
+++ src/main/java/com/itcz/czAIdada/controller/AppController.java
@@ -15,6 +15,7 @@ import com.itcz.czAIdada.model.dto.app.AppQueryRequest;
 import com.itcz.czAIdada.model.dto.app.AppUpdateRequest;
 import com.itcz.czAIdada.model.entity.App;
 import com.itcz.czAIdada.model.entity.User;
+import com.itcz.czAIdada.model.enums.ReviewStatusEnum;
 import com.itcz.czAIdada.model.vo.AppVO;
 import com.itcz.czAIdada.service.AppService;
 import com.itcz.czAIdada.service.UserService;
@@ -52,14 +53,15 @@ public class AppController {
     @PostMapping("/add")
     public BaseResponse<Long> addApp(@RequestBody AppAddRequest appAddRequest, HttpServletRequest request) {
         ThrowUtils.throwIf(appAddRequest == null, ErrorCode.PARAMS_ERROR);
-        // todo 在此处将实体类和 DTO 进行转换
+        //  在此处将实体类和 DTO 进行转换
         App app = new App();
         BeanUtils.copyProperties(appAddRequest, app);
         // 数据校验
         appService.validApp(app, true);
-        // todo 填充默认值
+        // 填充默认值
         User loginUser = userService.getLoginUser(request);
         app.setUserId(loginUser.getId());
+        app.setReviewStatus(ReviewStatusEnum.REVIEWING.getValue());
         // 写入数据库
         boolean result = appService.save(app);
         ThrowUtils.throwIf(!result, ErrorCode.OPERATION_ERROR);
@@ -107,7 +109,7 @@ public class AppController {
         if (appUpdateRequest == null || appUpdateRequest.getId() <= 0) {
             throw new BusinessException(ErrorCode.PARAMS_ERROR);
         }
-        // todo 在此处将实体类和 DTO 进行转换
+        //  在此处将实体类和 DTO 进行转换
         App app = new App();
         BeanUtils.copyProperties(appUpdateRequest, app);
         // 数据校验
@@ -213,7 +215,7 @@ public class AppController {
         if (appEditRequest == null || appEditRequest.getId() <= 0) {
             throw new BusinessException(ErrorCode.PARAMS_ERROR);
         }
-        // todo 在此处将实体类和 DTO 进行转换
+        //  在此处将实体类和 DTO 进行转换
         App app = new App();
         BeanUtils.copyProperties(appEditRequest, app);
         // 数据校验
@@ -227,6 +229,8 @@ public class AppController {
         if (!oldApp.getUserId().equals(loginUser.getId()) && !userService.isAdmin(loginUser)) {
             throw new BusinessException(ErrorCode.NO_AUTH_ERROR);
         }
+        //重置审核状态
+        app.setReviewStatus(ReviewStatusEnum.REVIEWING.getValue());
         // 操作数据库
         boolean result = appService.updateById(app);
         ThrowUtils.throwIf(!result, ErrorCode.OPERATION_ERROR);
--- src/main/java/com/itcz/czAIdada/controller/QuestionController.java
+++ src/main/java/com/itcz/czAIdada/controller/QuestionController.java
@@ -1,5 +1,6 @@
 package com.itcz.czAIdada.controller;
 
+import cn.hutool.json.JSONUtil;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.itcz.czAIdada.annotation.AuthCheck;
 import com.itcz.czAIdada.common.BaseResponse;
@@ -9,10 +10,7 @@ import com.itcz.czAIdada.common.ResultUtils;
 import com.itcz.czAIdada.constant.UserConstant;
 import com.itcz.czAIdada.exception.BusinessException;
 import com.itcz.czAIdada.exception.ThrowUtils;
-import com.itcz.czAIdada.model.dto.question.QuestionAddRequest;
-import com.itcz.czAIdada.model.dto.question.QuestionEditRequest;
-import com.itcz.czAIdada.model.dto.question.QuestionQueryRequest;
-import com.itcz.czAIdada.model.dto.question.QuestionUpdateRequest;
+import com.itcz.czAIdada.model.dto.question.*;
 import com.itcz.czAIdada.model.entity.Question;
 import com.itcz.czAIdada.model.entity.User;
 import com.itcz.czAIdada.model.vo.QuestionVO;
@@ -24,6 +22,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 /**
  * 题目接口
@@ -52,12 +51,14 @@ public class QuestionController {
     @PostMapping("/add")
     public BaseResponse<Long> addQuestion(@RequestBody QuestionAddRequest questionAddRequest, HttpServletRequest request) {
         ThrowUtils.throwIf(questionAddRequest == null, ErrorCode.PARAMS_ERROR);
-        // todo 在此处将实体类和 DTO 进行转换
+        // 在此处将实体类和 DTO 进行转换
         Question question = new Question();
         BeanUtils.copyProperties(questionAddRequest, question);
+        List<QuestionContentDTO> questionContent = questionAddRequest.getQuestionContent();
+        question.setQuestionContent(JSONUtil.toJsonStr(questionContent));
         // 数据校验
         questionService.validQuestion(question, true);
-        // todo 填充默认值
+        // 填充默认值
         User loginUser = userService.getLoginUser(request);
         question.setUserId(loginUser.getId());
         // 写入数据库
@@ -107,9 +108,11 @@ public class QuestionController {
         if (questionUpdateRequest == null || questionUpdateRequest.getId() <= 0) {
             throw new BusinessException(ErrorCode.PARAMS_ERROR);
         }
-        // todo 在此处将实体类和 DTO 进行转换
+        // 在此处将实体类和 DTO 进行转换
         Question question = new Question();
         BeanUtils.copyProperties(questionUpdateRequest, question);
+        List<QuestionContentDTO> questionContent = questionUpdateRequest.getQuestionContent();
+        question.setQuestionContent(JSONUtil.toJsonStr(questionContent));
         // 数据校验
         questionService.validQuestion(question, false);
         // 判断是否存在
@@ -213,9 +216,11 @@ public class QuestionController {
         if (questionEditRequest == null || questionEditRequest.getId() <= 0) {
             throw new BusinessException(ErrorCode.PARAMS_ERROR);
         }
-        // todo 在此处将实体类和 DTO 进行转换
+        // 在此处将实体类和 DTO 进行转换
         Question question = new Question();
         BeanUtils.copyProperties(questionEditRequest, question);
+        List<QuestionContentDTO> questionContent = questionEditRequest.getQuestionContent();
+        question.setQuestionContent(JSONUtil.toJsonStr(questionContent));
         // 数据校验
         questionService.validQuestion(question, false);
         User loginUser = userService.getLoginUser(request);
