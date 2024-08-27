9ece99e672bdf0665f312aa5337522095d89d4f0
cz <3302076969@qq.com>
2024-08-27 16:48:03 +0800

    引入智谱AI大模型的SDK，并进行AI通用模块的封装

---
+++ src/main/java/com/itcz/czAIdada/constant/KeyConstant.java
@@ -0,0 +1,5 @@
+package com.itcz.czAIdada.constant;
+
+public interface KeyConstant {
+    public static final String KEY = "07e042bc6775cd61cf7e9594fd781004.H9REk01AnYodSHAL";
+}
---
+++ src/main/java/com/itcz/czAIdada/manager/AiManager.java
@@ -0,0 +1,116 @@
+package com.itcz.czAIdada.manager;
+
+import com.itcz.czAIdada.common.ErrorCode;
+import com.itcz.czAIdada.exception.BusinessException;
+import com.zhipu.oapi.ClientV4;
+import com.zhipu.oapi.Constants;
+import com.zhipu.oapi.service.v4.model.ChatCompletionRequest;
+import com.zhipu.oapi.service.v4.model.ChatMessage;
+import com.zhipu.oapi.service.v4.model.ChatMessageRole;
+import com.zhipu.oapi.service.v4.model.ModelApiResponse;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 通用 AI 调用能力
+ */
+@Component
+public class AiManager {
+
+    @Resource
+    private ClientV4 clientV4;
+
+
+    // 稳定的随机数
+    private static final float STABLE_TEMPERATURE = 0.05f;
+
+    // 不稳定的随机数
+    private static final float UNSTABLE_TEMPERATURE = 0.99f;
+
+
+   /* //构建请求
+    //List<ChatMessage> messages = new ArrayList<>();
+    ChatMessage chatMessage = new ChatMessage(ChatMessageRole.USER.value(), "作为一名营销专家，请为智谱开放平台创作一个吸引人的slogan");
+        messages.add(chatMessage);*/
+
+    /**
+     * 通用请求
+     * @param messages
+     * @param stream
+     * @param temperature
+     * @return
+     */
+    public String doRequest(List<ChatMessage> messages,Boolean stream,Float temperature){
+        //构造请求
+        ChatCompletionRequest chatCompletionRequest = ChatCompletionRequest.builder()
+                .model(Constants.ModelChatGLM4)
+                .stream(stream)
+                .temperature(temperature)
+                .invokeMethod(Constants.invokeMethod)
+                .messages(messages)
+                //.requestId(requestId)
+                .build();
+        try {
+            ModelApiResponse invokeModelApiResp = clientV4.invokeModelApi(chatCompletionRequest);
+            return invokeModelApiResp.getData().getChoices().get(0).toString();
+        }catch (Exception e){
+            e.printStackTrace();
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR,e.getMessage());
+        }
+    }
+
+    /**
+     * 通用请求（简化消息传递）
+     *
+     * @param systemMessage
+     * @param userMessage
+     * @param stream
+     * @param temperature
+     * @return
+     */
+    public String doRequest(String systemMessage, String userMessage, Boolean stream, Float temperature) {
+        List<ChatMessage> chatMessageList = new ArrayList<>();
+        ChatMessage systemChatMessage = new ChatMessage(ChatMessageRole.SYSTEM.value(), systemMessage);
+        chatMessageList.add(systemChatMessage);
+        ChatMessage userChatMessage = new ChatMessage(ChatMessageRole.USER.value(), userMessage);
+        chatMessageList.add(userChatMessage);
+        return doRequest(chatMessageList, stream, temperature);
+    }
+
+    /**
+     * 同步请求
+     *
+     * @param systemMessage
+     * @param userMessage
+     * @param temperature
+     * @return
+     */
+    public String doSyncRequest(String systemMessage, String userMessage, Float temperature) {
+        return doRequest(systemMessage, userMessage, Boolean.FALSE, temperature);
+    }
+
+    /**
+     * 同步请求（答案不稳定）
+     *
+     * @param systemMessage
+     * @param userMessage
+     * @return
+     */
+    public String doSyncUnstableRequest(String systemMessage, String userMessage) {
+        return doRequest(systemMessage, userMessage, Boolean.FALSE, UNSTABLE_TEMPERATURE);
+    }
+
+    /**
+     * 同步请求（答案较稳定）
+     *
+     * @param systemMessage
+     * @param userMessage
+     * @return
+     */
+    public String doSyncStableRequest(String systemMessage, String userMessage) {
+        return doRequest(systemMessage, userMessage, Boolean.FALSE, STABLE_TEMPERATURE);
+    }
+}
